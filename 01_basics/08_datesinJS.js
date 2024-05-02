d7858e4b1b86debae945722f8bafd2cd30610ca3
amitraj03 <127528790+amitraj03@users.noreply.github.com>
2024-05-02 13:59:47 +0000

    datesinJS

--- 01_basics/08_datesinJS.js
+++ 01_basics/08_datesinJS.js
@@ -14,5 +14,11 @@ let myCreatedDate = new Date("02-05-2024")
 
 let myTimeStamp = Date.now()
 
-console.log(myTimeStamp);
-console.log(myCreatedDate.getDate)
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
